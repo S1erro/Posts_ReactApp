9abe4403c7b25e7a546533fe04593f20666178bc
Аслан Аслан_Асланов <riviev92@mail.ru>
2024-05-07 13:54:43 +0300

    small changes

--- src/App.js
+++ src/App.js
@@ -5,44 +5,46 @@ import MyButton from "./Componets/UI/button/MyButton";
 import MyInput from "./Componets/UI/input/MyInput";
 
 function App() {
+    // Хранит все посты на странице
     const [posts, setPosts] = React.useState([
         {id: 1, title: 'Javascript', description: 'This language is not as easy as I thought it would be.....'},
         {id: 2, title: 'Python', description: 'Best times in my life'},
         {id: 3, title: 'C++', description: 'You lost 2gb or RAM because of carelessness'}
     ]);
 
-    const [title, setTitle] = React.useState('');
+    // Состояние двух input (какие данные введены в строки)
+    const [post, setPost] = React.useState({title: '', description: ''});
 
-    const [description, setDescription] = React.useState('');
-
-    const addPost = () => {
+    // Добавление нового поста
+    const addPost = (e) => {
+        e.preventDefault()
         const newPost = {
             id: Date.now(),
-            title,
-            description
+            ...post
         }
         setPosts([...posts, newPost])
-
-        setTitle('')
-        setDescription('')
+        //setPosts([...posts, {...post, id: Date.now()}])
+        setPost({...post, title: '', description: ''})
     }
 
     return (
         <div className="App">
-            <MyInput
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                type="text"
-                placeholder="Введите заголовок новой записи"
-            />
-            <MyInput
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-                type="text"
-                placeholder="Введите текст новой записи"
-            />
-            <MyButton onClick={addPost}>Записать</MyButton>
-            <AllPosts posts={posts} title={"Записи:"}/>
+            <form>
+                <MyInput
+                    value={post.title}
+                    onChange={(e) => setPost({...post, title: e.target.value})}
+                    type="text"
+                    placeholder="Введите заголовок новой записи"
+                />
+                <MyInput
+                    value={post.description}
+                    onChange={(e) => setPost({...post, description: e.target.value})}
+                    type="text"
+                    placeholder="Введите текст новой записи"
+                />
+                <MyButton onClick={addPost}>Записать</MyButton>
+                <AllPosts posts={posts} title={"Записи:"}/>
+            </form>
         </div>
     );
 }
